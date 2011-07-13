728d9371b711642f498000e746abdf80c7f67fe9
Tim Nelson <tnelly27@gmail.com>
2011-07-13 15:12:28 -0500

    Added publishing config

--- project/build/ScalaxmlProject.scala
+++ project/build/ScalaxmlProject.scala
@@ -4,6 +4,24 @@ class ScalaxmlProject(info: ProjectInfo) extends DefaultProject(info) {
 
 	val tagsoup = "org.ccil.cowan.tagsoup" % "tagsoup" % "1.2" % "compile->default"
 
-	val specs = "org.scala-tools.testing" %% "specs" % "1.6.6" % "test->default"
+	val specs = "org.scala-tools.testing" %% "specs" % "1.6.8" % "test->default"
 	val junit = "junit" % "junit" % "4.5" % "test->default"
+
+	/**
+   * Test if project is a SNAPSHOT build.
+   */
+  def isSnapshot = version.toString.endsWith("-SNAPSHOT")
+
+  lazy val localRepo = Path.userHome / "projects" / "eltimn-mvn-repo"
+
+	object PublishRepositories {
+    lazy val snapshot = Resolver.file("eltimn Maven Repo for Snapshots", localRepo / "snapshots" asFile)
+    lazy val release  = Resolver.file("eltimn Maven Repo for Releases", localRepo / "releases" asFile)
+  }
+
+  override def managedStyle = ManagedStyle.Maven
+
+  lazy val publishTo =
+    if (isSnapshot) PublishRepositories.snapshot
+    else PublishRepositories.release
 }
